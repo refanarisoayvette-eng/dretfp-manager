164076c79040f109b9035c8caa2749a7595ab9fd
refanarisoaoyette-eng <ton.email@gmail.com>
2026-09-17 18:16:39 +0300

    Auto-create admin on deploy

--- build.sh
+++ build.sh
@@ -6,3 +6,16 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Creer automatiquement l'admin
+python manage.py shell << 'EOF'
+from django.contrib.auth.models import User
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@dretfp.mg', 'Dretfp2027!')
+    print('OK : Admin cree')
+else:
+    print('OK : Admin existe deja')
+EOF
+
+echo "BUILD TERMINE !"
